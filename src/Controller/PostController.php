9a77ba0b4748784283b30a69684d13bb6e83a52d
Konstantin <kostyannagula@gmail.com>
2017-03-15 18:23:26 +0200

    REST crud

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,6 +9,7 @@
 namespace Acme\Controller;
 
 use App\RequestMethod;
+use App\PostCRUD;
 
 class PostController
 {
@@ -28,11 +29,35 @@ class PostController
 
     public function getAction(int $id)
     {
-        return 'get post ' . $id;
+        $crud = new PostCRUD();
+
+        $post = $crud->getOne($id);
+
+        $json = json_encode($post);
+
+        if (!$json) {
+            echo 'fail';
+            return false;
+        }
+
+        header('Content-Type: application/json');
+
+        echo $json;
     }
 
     public function postAction()
     {
+        $crud = new PostCRUD();
+
+        $json = file_get_contents('php://input');
+        $data = json_decode($json);
+
+        $title = $data->title;
+        $author = $data->title;
+        $content = $data->content;
+
+        $crud->create($title, $content, $author);
+
         return 'create new post';
     }
 
@@ -43,6 +68,9 @@ class PostController
 
     public function deleteAction(int $id)
     {
+        $crud = new PostCRUD();
+
+        $crud->delete($id);
         return 'delete post' . $id;
     }
 }
